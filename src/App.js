1fc9bfb821167ee9fee9d4da192a94b1cdf2bac5
Joona Lusa <joona.lusa@hotmail.com>
2022-03-09 20:07:06 -0500

    added a background image

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import LandingPage from './components/LandingPage';
 
 function App() {
   return (
-      <><link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,900&display=swap" rel="stylesheet"></link><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous"></link><Header /><LandingPage /></>
+      <><Header /><LandingPage /></>
   );
 }
 
